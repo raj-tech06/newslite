fdf929abe79c79ddefc54449e6d0a969174b3865
Abhishek Sahu <rjdeveloper07@gmail.com>
2025-08-01 22:53:38 +0530

    modify

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -6,12 +6,12 @@ from .models import News
 class NewsForm(forms.ModelForm):
     class Meta:
         model = News
-        fields = ['title', 'content', 'category', 'image', 'video']
+        fields = ['title', 'content', 'category', 'image']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
             'category': forms.Select(attrs={'class': 'form-select'}),
             'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
-            'video': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            # 'video': forms.ClearableFileInput(attrs={'class': 'form-control'}),
         }
         
